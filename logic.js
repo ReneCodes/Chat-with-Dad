06a840644a8ac5e858589060ac73575936df2670
Rene G <r.gromeier@gmail.com>
2023-02-28 14:17:51 +0000

    inlcuded scroll-last-message logic and nimation

--- logic.js
+++ logic.js
@@ -73,8 +73,9 @@
 */
 
 $(document).ready(function () {
+  scrollToLastMessage();
   // Creates a new <audio> element
-  var $sound = $('<audio preload=auto>');
+  const $sound = $('<audio preload=auto>');
   // See below for more sound URLs you can use
   $sound.attr(
     'src',
@@ -85,6 +86,7 @@ $(document).ready(function () {
   $('.btn').on('click', function () {
     $sound[0].play();
     buildMessageBlock('This is a test message', 'right');
+    getTimestamp();
   });
 
   $('#send-text').on('submit', function (event) {
@@ -108,10 +110,41 @@ $(document).ready(function () {
     $timestamp.addClass('timestamp').text('timestamp').appendTo($block);
     $message.append($block);
     $('.message-container').append($message);
-    // scroll down to end of message block
-
+    // scroll down to end last message
+    scrollToLastMessage(true);
     //clear message in textarea
     $('#input')[0].value = '';
     $('#input')[0].focus();
+  }
+  /* TODO
+    get date
+    extract day, month, year
+    extract hour, minutes
+    create timestamp block
+    append to text-block
+  */
+  function getTimestamp() {
+    // console.log('timestamp creation');
+  }
+
+  /* TODO
+    DONE: scrolling behaviour -> smooth
+    DONE: scroll to last item in container
+  */
+  function scrollToLastMessage(addText = false) {
+    console.log($('.message').last()[0].offsetTop);
+    $('.message-container').animate(
+      { scrollTop: $('.message').last().offset().top },
+      {
+        duration: 1600,
+        queue: false, // run animation outside queue, start without waiting
+      }
+    );
+    // only fade-in newly added messages
+    if (addText) {
+      $('.message').animate({ opacity: 1 }, { duration: 1000 });
+    } else {
+      $('.message').css('opacity', 1);
+    }
   }
 });
